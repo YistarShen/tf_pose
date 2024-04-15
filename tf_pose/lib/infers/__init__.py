9d59bd35a2dde47d6ad19eef7d6a881216e5e3a1
YistarShen <yistaropip@gmail.com>
2024-04-15 17:11:38 +0800

    remove infer
    remove infer due to not ready

--- tf_pose/lib/infers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import BaseProc
-from .proc_det import PreProcess_Det, PostProcess_YOLO_Det
-from .proc_pose import PreProcess_Pose, PostProcess_HM_Pose, PostProcess_SIMCC_Pose
-from .proc_lifter import PreProcess_Lifter, PostProcess_Lifter
-__all__ = ['BaseProc', 
-           'PreProcess_Det', 'PostProcess_YOLO_Det',
-           'PreProcess_Pose', 'PostProcess_HM_Pose', 'PostProcess_SIMCC_Pose',
-           'PreProcess_Lifter', 'PostProcess_Lifter']
